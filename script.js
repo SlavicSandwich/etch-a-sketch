1fa9adcf8ea4d0c2afdfd3ff3e0b381147681d5c
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-05-31 00:08:55 +0300

    add color picker and color change in grid

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const grid = document.querySelector('.grid-container');
-const restart = document.querySelector('.restart');
-restart.addEventListener('click', reset_grid)
-
-
-for (let i = 0; i < 256; i++){
-    grid_block = document.createElement('div');
-    grid_block.classList.add('grid-block');
-    grid.appendChild(grid_block);
+function reset_grid() {
+  const blocks = grid.querySelectorAll(".grid-block");
+  blocks.forEach((block) => reset_color(block));
 }
 
-function reset_grid(){
-    const blocks = grid.querySelectorAll('.grid-block');
-    blocks.forEach((block)=>(reset_color(block)))
+function reset_color(block) {
+  block.style.backgroundColor = "white";
 }
 
-function reset_color(block){
-    block.style.backgroundColor = 'white';
+function change_color(color) {
+  this.style.backgroundColor = colorpicker.value;
 }
 
-function change_color(){
-    this.style.backgroundColor = 'black';
+const grid = document.querySelector(".grid-container");
+for (let i = 0; i < 256; i++) {
+  grid_block = document.createElement("div");
+  grid_block.classList.add("grid-block");
+  grid.appendChild(grid_block);
 }
-const blocks = grid.querySelectorAll('.grid-block');
-blocks.forEach((block)=>{block.addEventListener("mouseover", change_color)})
+const colorpicker = document.querySelector("#colorpicker");
+const blocks = grid.querySelectorAll(".grid-block");
+blocks.forEach((block) => {
+  block.addEventListener("mouseover", change_color);
+});
+
+const restart = document.querySelector(".restart");
+restart.addEventListener("click", reset_grid);
+
+
