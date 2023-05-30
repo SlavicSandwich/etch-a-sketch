d983c70456cefa689595c1348253a59910b73069
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-05-30 23:52:19 +0300

    add basic restart button

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const grid = document.querySelector('.grid-container');
+const restart = document.querySelector('.restart');
+restart.addEventListener('click', reset_grid)
+
 
 for (let i = 0; i < 256; i++){
     grid_block = document.createElement('div');
     grid_block.classList.add('grid-block');
     grid.appendChild(grid_block);
+}
+
+function reset_grid(){
+    const blocks = grid.querySelectorAll('.grid-block');
+    blocks.forEach((block)=>(reset_color(block)))
+}
+
+function reset_color(block){
+    block.style.backgroundColor = 'white';
 }
 
 function change_color(){
